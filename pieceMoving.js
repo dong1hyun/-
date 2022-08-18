ca314280941b08e189da45938fdd59de7f781539
dong1hyun <limd1238@naver.com>
2022-08-18 21:14:00 +0900

    폰 이동 상대편 공격까지 완성

--- pieceMoving.js
+++ pieceMoving.js
@@ -1,5 +1,5 @@
 const whitePawn = document.querySelectorAll('#whitePawn');
-const blackPawn = document.querySelector('#blackPawn');
+const blackPawn = document.querySelectorAll('#blackPawn');
 let movingTarget;
 let clickedNum = -1;  //클릭된 말의 번호 (클릭 되지 않았으면 -1)
 
@@ -12,18 +12,25 @@ function clearMovingPoint() {
 
 for (i = 0; i < 64; i++) {
     const movingPoint = tiles[i].firstElementChild;
+    
     movingPoint.onclick = function () {
         clearMovingPoint();
+        if(movingTarget.getAttribute('class') == 'whiteTeam' && this.nextElementSibling != null && this.nextElementSibling.getAttribute('class') == 'blackTeam'){
+            movingPoint.parentElement.removeChild(this.nextElementSibling);
+        }
+        if(movingTarget.getAttribute('class') == 'blackTeam' && this.nextElementSibling != null && this.nextElementSibling.getAttribute('class') == 'whiteTeam'){
+            movingPoint.parentElement.removeChild(this.nextElementSibling);
+        }
         this.parentNode.appendChild(movingTarget);
     }
 }
 
-function pawnMove() {
+function whitePawnMove() {
     for (i = 0; i < whitePawn.length; i++) {
         whitePawn[i].onclick = function () {
-            
             movingTarget = this;
             const tileNum = this.parentElement.getAttribute('value');
+        
             if (tileNum == clickedNum) {
                 clearMovingPoint();
                 clickedNum = -1;
@@ -31,13 +38,22 @@ function pawnMove() {
             }
            
             clearMovingPoint(); 
+            if(movingTarget.getAttribute('id') == 'blackPawn' || movingTarget.getAttribute('id') == 'whitePawn'){
+                if(tiles[tileNum - 7].lastElementChild != null && tiles[tileNum - 7].lastElementChild.getAttribute('class') == 'blackTeam'){
+                    tiles[tileNum - 7].firstElementChild.style.display = 'block';
+                }
+                if(tiles[tileNum - 9].lastElementChild != null && tiles[tileNum - 9].lastElementChild.getAttribute('class') == 'blackTeam'){
+                    tiles[tileNum - 9].firstElementChild.style.display = 'block';
+                }
+            }
+
             const ch1 = tiles[tileNum - 8].children;
             const ch2 = tiles[tileNum - 16].children;
-            if(ch1[1] != null && ch1[1].getAttribute('class') == 'whiteTeam'){
+            if(ch1[1] != null){ 
                 return;
             }
             tiles[tileNum - 8].firstElementChild.style.display = 'block';
-            if(ch2[1] != null && ch2[1].getAttribute('class') == 'whiteTeam'){
+            if(ch2[1] != null){
                 return;
             }
             tiles[tileNum - 16].firstElementChild.style.display = 'block';
@@ -47,4 +63,43 @@ function pawnMove() {
 }
 
 
-pawnMove();
+function blackePawnMove() {
+    for (i = 0; i < blackPawn.length; i++) {
+        blackPawn[i].onclick = function () {
+            movingTarget = this;
+            const tileNum = this.parentElement.getAttribute('value');
+            if (tileNum == clickedNum) {
+                clearMovingPoint();
+                clickedNum = -1;
+                return;
+            }
+            
+            clearMovingPoint(); 
+            if(movingTarget.getAttribute('id') == 'blackPawn' || movingTarget.getAttribute('id') == 'whitePawn'){
+                if(tiles[+tileNum + 7].lastElementChild != null && tiles[+tileNum + 7].lastElementChild.getAttribute('class') == 'whiteTeam'){
+                    tiles[+tileNum + 7].firstElementChild.style.display = 'block';
+                }
+                if(tiles[+tileNum + 9].lastElementChild != null && tiles[+tileNum + 9].lastElementChild.getAttribute('class') == 'whiteTeam'){
+                    tiles[+tileNum + 9].firstElementChild.style.display = 'block';
+                }
+            }
+
+            const ch1 = tiles[+tileNum + 8].children;
+            const ch2 = tiles[+tileNum + 16].children;
+            
+            if(ch1[1] != null){
+                return;
+            }   
+            tiles[+tileNum + 8].firstElementChild.style.display = 'block';
+            if(ch2[1] != null){
+                return;
+            }
+            tiles[+tileNum + 16].firstElementChild.style.display = 'block';
+            clickedNum = +tileNum;
+        }
+    }
+}
+
+
+whitePawnMove();
+blackePawnMove();
